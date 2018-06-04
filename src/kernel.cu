fb8554af0f44f2740a2d1408f90fb38e90375f93
Liraal2 <nowyadr@gmail.com>
2018-06-04 23:46:44 +0200

    device_graph and device_pheromone are now 1-dimensional

--- src/kernel.cu
+++ src/kernel.cu
@@ -11,46 +11,46 @@ const float ACOdelta = 0.1;//decrease
 const float ACOgamma = 0.1;//minval
 const int block_size = 10;
 
-__global__ void clique_kernel(int *A, int N, char **device_graph, float **device_pheromone, curandState *states) {
+__global__ void clique_kernel(int *A, int N, char *device_graph, float *device_pheromone, curandState *states) {
 	int id = blockIdx.x*blockDim.x;
     curand_init(1342, id, 0, &states[id]);
 	int startIdx = curand_uniform(&states[id])*N;
 	actual_device_vector<int> C(N/2), B(N/2);
-	for(int i = 0; i < N; i++) if(device_graph[startIdx][i]) B.push_back(i);
+	for(int i = 0; i < N; i++) if(device_graph[startIdx*N+i]) B.push_back(i);
 	C.push_back(startIdx); //END SETUP
 	int current = startIdx;
 	while(B.size()>0){ //MAIN LOOP
 		float norm = 0.0f;
-		for(int i = 0; i < B.size(); i++) norm += device_pheromone[current][B[i]];
+		for(int i = 0; i < B.size(); i++) norm += device_pheromone[current*N+B[i]];
 		int chosen = 0;
 		float radom = curand_uniform(&states[id]);
-		for(chosen=0;chosen < B.size() || radom<=0;chosen++,radom-= device_pheromone[current][B[chosen]]/norm); //NEXT VERTEX PICKED
-		for(int i = 0; i < B.size(); i++) if(!device_graph[chosen][B[i]]) B.erase(i); //REMOVE NON-NEIGHBORING
+		for(chosen=0;chosen < B.size() || radom<=0;chosen++,radom-= device_pheromone[current*N+B[chosen]]/norm); //NEXT VERTEX PICKED
+		for(int i = 0; i < B.size(); i++) if(!device_graph[chosen*N+B[i]]) B.erase(i); //REMOVE NON-NEIGHBORING
 		current = chosen;
 		C.push_back(chosen);
 	}
 	for(int i = 0; i < C.size()-1; i++) {
-		device_pheromone[C[i]][C[i+1]]=device_pheromone[C[i]][C[i+1]]+ACOalpha;
-		device_pheromone[C[i+1]][C[i]]=device_pheromone[C[i]][C[i+1]];
+		device_pheromone[C[i]*N+C[i+1]]=device_pheromone[C[i]*N+C[i+1]]+ACOalpha;
+		device_pheromone[C[i+1]*N+C[i]]=device_pheromone[C[i]*N+C[i+1]];
 	}
 	A[id]=C.size();
 }
-__global__ void evaporation_kernel(int N, float **device_pheromone){
+__global__ void evaporation_kernel(int N, float *device_pheromone){
 	int row = blockIdx.x *blockDim.x + threadIdx.x;
 	for(int i = 0; i<N; i++) {
-		device_pheromone[row][i] -= ACOdelta;
-		if(device_pheromone[row][i]<ACOgamma) device_pheromone[row][i]=ACOgamma;
+		device_pheromone[row*N+i] -= ACOdelta;
+		if(device_pheromone[row*N+i]<ACOgamma) device_pheromone[row*N+i]=ACOgamma;
 	}
 }
 
 extern "C" int anthill(char **graph, int N, int M){
 	curandState *states;
-	char **device_graph;
-	float **device_pheromone;
+	char *device_graph;
+	float *device_pheromone;
 	cudaMalloc(&states, N*sizeof(curandState));
 	cudaMalloc(&device_graph, N*N*sizeof(char));
 	cudaMalloc(&device_pheromone, N*N*sizeof(float));
-	cudaMemcpy(device_graph, graph, N*N*sizeof(char), cudaMemcpyHostToDevice); //graph initialized
+	cudaMemcpy(device_graph, graph[0], N*N*sizeof(char), cudaMemcpyHostToDevice); //graph initialized
 	int *results, *host_results=new int[block_size], max = 0;
 	cudaMalloc(&results, block_size*sizeof(int));
 	for(int i = 0; i < M; i++){
