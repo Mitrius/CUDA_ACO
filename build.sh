c66eca6f1c20250a0610f6faa7dcec2c5e2d307a
Liraal2 <nowyadr@gmail.com>
2018-06-04 22:05:19 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #module load mpi/mpich-x86_64
-mpicc -c src/main.cpp -o main.o -std=c++11
+mpic++ -c src/main.cpp -o main.o -std=c++11
 nvcc -c src/kernel.cu -o kernel.o
-mpicc main.o kernel.o -L/usr/local/cuda/lib64 -lcudart -lcurand -o main.out
+mpic++ kernel.o main.o -std=c++11 -L/usr/local/cuda/lib64 -o main.out -lcudart -lcurand
 chmod +x ./main.out
