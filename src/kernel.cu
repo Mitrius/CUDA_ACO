13f80426f3fcc57bcab438b3016dcde7f65ee01c
Liraal2 <nowyadr@gmail.com>
2018-06-05 13:04:03 +0200

    fixed "lol loops" error with graph processing random seeds are now based on system time

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <cstdio>
+#include <ctime>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <curand_kernel.h>
@@ -11,21 +12,21 @@ const float ACOdelta = 0.1;//decrease
 const float ACOgamma = 0.1;//minval
 const int block_size = 10;
 
-__global__ void clique_kernel(int *A, int N, char *device_graph, float *device_pheromone, curandState *states) {
+__global__ void clique_kernel(int *A, int N, char *device_graph, float *device_pheromone, curandState *states, unsigned int seed) {
 	int id = blockIdx.x*blockDim.x;
-    curand_init(1342, id, 0, &states[id]);
-	int startIdx = curand_uniform(&states[id])*N;
+    curand_init(seed*id, id, 0, &states[id]);
+	int startIdx = (1-curand_uniform(&states[id]))*N;
 	actual_device_vector<int> C(N/2), B(N/2);
-	for(int i = 0; i < N; i++) if(device_graph[startIdx*N+i]) B.push_back(i);
+	for(int i = 0; i < N; i++) if(i!=startIdx && device_graph[startIdx*N+i]) B.push_back(i);
 	C.push_back(startIdx); //END SETUP
 	int current = startIdx;
 	while(B.size()>0){ //MAIN LOOP
 		float norm = 0.0f;
 		for(int i = 0; i < B.size(); i++) norm += device_pheromone[current*N+B[i]];
 		int chosen = 0;
-		float radom = curand_uniform(&states[id]);
-		for(chosen=0;chosen < B.size() || radom<=0;chosen++,radom-= device_pheromone[current*N+B[chosen]]/norm); //NEXT VERTEX PICKED
-		for(int i = 0; i < B.size(); i++) if(!device_graph[chosen*N+B[i]]) B.erase(i); //REMOVE NON-NEIGHBORING
+		float radom = (1-curand_uniform(&states[id]));
+		for(chosen=0;chosen < B.size() && radom<=0;chosen++,radom-= device_pheromone[current*N+B[chosen]]/norm); //NEXT VERTEX PICKED
+		for(int i = 0; i < B.size(); i++) if(i==chosen || !device_graph[chosen*N+B[i]]) B.erase(i); //REMOVE NON-NEIGHBORING
 		current = chosen;
 		C.push_back(chosen);
 	}
@@ -42,7 +43,6 @@ __global__ void evaporation_kernel(int N, float *device_pheromone){
 		if(device_pheromone[row*N+i]<ACOgamma) device_pheromone[row*N+i]=ACOgamma;
 	}
 }
-
 extern "C" int anthill(char **graph, int N, int M){
 	curandState *states;
 	char *device_graph;
@@ -55,8 +55,8 @@ extern "C" int anthill(char **graph, int N, int M){
 	cudaMalloc(&results, block_size*sizeof(int));
 	for(int i = 0; i < M; i++){
 		evaporation_kernel<<<N,1>>>(N, device_pheromone);
-		clique_kernel<<<block_size,1>>>(results, N, device_graph, device_pheromone, states);
-		printf("%s ", cudaGetErrorName(cudaMemcpy(host_results, results, block_size*sizeof(int), cudaMemcpyDeviceToHost)));
+		clique_kernel<<<block_size,1>>>(results, N, device_graph, device_pheromone, states, (unsigned int)time(NULL));
+		cudaMemcpy(host_results, results, block_size*sizeof(int), cudaMemcpyDeviceToHost);
 		for(int i = 0; i < block_size; i++) if(max<host_results[i]) max = host_results[i];
 	}
 	delete[] host_results;
